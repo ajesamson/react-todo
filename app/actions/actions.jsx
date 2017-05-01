b320451735546e8042830e5ce8c3bc40eb8c17ce
Samson Alajede <samson.alajede@konga.com>
2017-05-01 14:03:44 +0100

    Update todos on firebase

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -51,9 +51,31 @@ export var toggleShowCompleted = () => {
   }
 };
 
-export var toggleTodo = (id) => {
+// export var toggleTodo = (id) => {
+//   return {
+//     type: 'TOGGLE_TODO',
+//     id
+//   }
+// };
+
+export var updateTodo = (id, updates) => {
   return {
-    type: 'TOGGLE_TODO',
-    id
+    type: 'UPDATE_TODO',
+    id,
+    updates
   }
+};
+
+export var startToggleTodo = (id, completed) => {
+  return (dispatch, getState) => {
+    var todoRef = firebaseRef.child(`todos/${id}`);
+    var updates = {
+      completed,
+      completedAt: completed ? moment().unix() : null
+    }
+
+    return todoRef.update(updates).then(() => {
+      dispatch(updateTodo(id, updates));
+    });
+  };
 };
