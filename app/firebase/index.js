978ab25faf853ac0339bbe5fec7e54c4192f9b84
Samson Alajede <samson.alajede@konga.com>
2017-05-02 11:12:33 +0100

    Move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-      apiKey: "AIzaSyBVKWZGqOX9714TLuT_wBE-CykQ7FbM-g8",
-      authDomain: "fhitsolutions-todo-app.firebaseapp.com",
-      databaseURL: "https://fhitsolutions-todo-app.firebaseio.com",
-      projectId: "fhitsolutions-todo-app",
-      storageBucket: "fhitsolutions-todo-app.appspot.com",
-      messagingSenderId: "601650814063"
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID
     };
 
   firebase.initializeApp(config);
